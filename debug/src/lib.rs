537ab346ecdec07e957ed801b4a0eb6ab8a2f899
Marat Khusnetdinov <marat@khusnetdinov.ru>
2024-04-02 00:02:28 +0300

    # 1

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,24 @@
-use proc_macro::TokenStream;
+use proc_macro_error::proc_macro_error;
 
+#[proc_macro_error]
 #[proc_macro_derive(CustomDebug)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let ast: syn::DeriveInput = syn::parse_macro_input!(input);
 
-    unimplemented!()
+    // dbg!(&ast);
+
+    let _name = &ast.ident;
+    // let data_struct = match &ast.data {
+    //     syn::Data::Struct(data_struct) => data_struct,
+    //     _ => abort_call_site!("expected struct"),
+    // };
+
+    // let _named = match data_struct.fields {
+    //     syn::Fields::Named(fields) => fields.named,
+    //     _ => abort_call_site!("expected struct with named fields"),
+    // };
+
+    let quote = quote::quote! {};
+
+    proc_macro::TokenStream::from(quote)
 }
