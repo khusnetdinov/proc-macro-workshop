e8b6135b38eb7808b30c33fbddc3c73d6ad55a74
Marat Khusnetdinov <marat@khusnetdinov.ru>
2024-04-02 23:13:04 +0300

    # 3

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,7 +1,7 @@
 use proc_macro_error::{abort_call_site, proc_macro_error};
 
 #[proc_macro_error]
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast: syn::DeriveInput = syn::parse_macro_input!(input);
 
@@ -20,11 +20,32 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
     let struct_fields_names = named.iter().map(|field: &syn::Field| &field.ident);
 
+    let formatter_fields_names = named.iter().map(|field: &syn::Field| {
+        for attr in &field.attrs {
+            if attr.path().is_ident("debug") {
+                if let syn::Meta::NameValue(ref meta, ..) = attr.meta {
+                    if let syn::Expr::Lit(ref expr) = meta.value {
+                        if let syn::Lit::Str(ref lit_str) = expr.lit {
+                            return lit_str.value();
+                        }
+                    }
+                } else {
+                    abort_call_site!("attribute macro expected format `#[debug = \"formatter\"]`")
+                }
+            }
+        }
+
+        String::from("\"{}\"")
+    });
+
     let quote = quote::quote! {
         impl std::fmt::Debug for #struct_ident {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                 f.debug_struct(stringify!(#struct_ident))
-                    #(.field(stringify!(#struct_fields_names)c, &self.#struct_fields_names))*
+                    #(.field(
+                        stringify!(#struct_fields_names),
+                        &format_args!(#formatter_fields_names, &self.#struct_fields_names)
+                    ))*
                     .finish()
             }
         }
