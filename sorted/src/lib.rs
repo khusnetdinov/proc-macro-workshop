7464cdce6d805aae96f644d99c738f845076a82e
Marat Khusnetdinov <marat@khusnetdinov.ru>
2024-04-14 16:52:13 +0300

    # 3

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,3 +1,5 @@
+use syn::spanned::Spanned;
+
 #[proc_macro_attribute]
 pub fn sorted(
     args: proc_macro::TokenStream,
@@ -6,13 +8,36 @@ pub fn sorted(
     let _ = args;
     let item = syn::parse_macro_input!(input as syn::Item);
 
-    sorted_impl(item).into()
+    sorted_impl(item)
+        .unwrap_or_else(|e| e.to_compile_error())
+        .into()
 }
 
-fn sorted_impl(item: syn::Item) -> proc_macro2::TokenStream {
+fn sorted_impl(item: syn::Item) -> Result<proc_macro2::TokenStream, syn::Error> {
     if let syn::Item::Enum(item_enum) = item {
-        quote::quote! { #item_enum }
+        let mut names = Vec::new();
+
+        for variant in item_enum.variants.iter() {
+            let name = variant.ident.to_string();
+
+            if names.last().map(|last| &name < last).unwrap_or(false) {
+                let next_index = names.binary_search(&name).unwrap_err();
+                let should_be = &names[next_index];
+
+                return Err(syn::Error::new(
+                    variant.span(),
+                    format!("{name} should sort before {should_be}"),
+                ));
+            }
+
+            names.push(name)
+        }
+
+        Ok(quote::quote! { #item_enum })
     } else {
-        quote::quote! { compile_error!("expected enum or match expression"); }
+        Err(syn::Error::new(
+            proc_macro2::Span::call_site(),
+            "expected enum or match expression",
+        ))
     }
 }
