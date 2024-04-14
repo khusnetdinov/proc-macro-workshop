75d4b5c42e4036d199510d1a20b232224f5a54ec
Marat Khusnetdinov <marat@khusnetdinov.ru>
2024-04-14 16:15:26 +0300

    # 2

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,7 +1,18 @@
 #[proc_macro_attribute]
-pub fn sorted(args: proc_macro::TokenStream, input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+pub fn sorted(
+    args: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
     let _ = args;
-    let _output = input.clone();
+    let item = syn::parse_macro_input!(input as syn::Item);
 
-    input
+    sorted_impl(item).into()
+}
+
+fn sorted_impl(item: syn::Item) -> proc_macro2::TokenStream {
+    if let syn::Item::Enum(item_enum) = item {
+        quote::quote! { #item_enum }
+    } else {
+        quote::quote! { compile_error!("expected enum or match expression"); }
+    }
 }
